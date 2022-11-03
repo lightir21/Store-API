34f1ba72e117ea6c5522d577d3fde826500ab3b7
meir <lightir21@gmail.com>
2022-11-03 13:56:32 +0200

    Set Query Object
    products.js(controllers):
    	-inside of getAllProducts function
    		-destructure {featured} from req.query -> const { featured } = req.query; //this will destructure only the value we want out of the query params
    		-create a constant queryObj and set it to empty object // if it will not change then it will search for all products like a normal .find({}) method
    		-create an if statement and check if featured is true then set the featured property on the queryObj and create a ternary operator if featured is "true" then send true if not send false ->
    		if (featured) {
        	queryObj.featured = featured === "true" ? true : false;
      		}
      		-set the queryObj inside of the find method // then it will only check the specific values we decided, if a use insert a diffrent value we didn't handle he will get all the products

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,15 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ msg: products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const products = await Product.find(req.query);
+  const { featured } = req.query;
+
+  const queryObj = {};
+
+  if (featured) {
+    queryObj.featured = featured === "true" ? true : false;
+  }
+
+  const products = await Product.find(queryObj);
 
   res.status(200).json({ products, nbHits: products.length });
 };
