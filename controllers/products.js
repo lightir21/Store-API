9b08457c38371ec1e5868a3dae95262150053336
meir <lightir21@gmail.com>
2022-11-04 22:06:09 +0200

    Sort functionality
    products.js(controllers):
    	-inside getAllProducts function
    		-add sort to the destructure objext from req.query
    		-change const products to -> let result
    		-under the let create an if statement and check if there is a sort property
    		-if true create a constant sortList and set it to sort.split(",").join(" ") // basicaly what it does is to take the user input like "name,-price,createdAt" and splits it where the "," stands and create an array and then we join the array back to a big string with .join(" ") with empty spaces between each of the words
    		-set result equal result.sort(sortList) will chain the sort method
    		-create an else statement set it result equal result.sort("createdAt") // we basicly set a default sort functionality so it will be sorted by the time we decided
    		-then create a constant called products and set it equal to await result // tihs will return the awaited promise with all the sorting done allready

--- controllers/products.js
+++ controllers/products.js
@@ -5,7 +5,7 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ msg: products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
 
   const queryObj = {};
 
@@ -21,7 +21,15 @@ const getAllProducts = async (req, res) => {
     queryObj.name = { $regex: name, $options: "i" };
   }
 
-  const products = await Product.find(queryObj);
+  let result = Product.find(queryObj);
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  const products = await result;
 
   res.status(200).json({ products, nbHits: products.length });
 };
